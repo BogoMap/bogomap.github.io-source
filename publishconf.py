2fa717ad8e823b54aa6d066f74ba26991b21e9d3
Andres Gomez Casanova <angoca@yahoo.com>
2016-02-18 06:04:04 +0100

    URL completa

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'rutas.bogomap.co'
+SITEURL = 'http://rutas.bogomap.co'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
